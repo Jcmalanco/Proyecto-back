35fc087dc9d1e9e3088cdd8d35724577bf60cd02
Jcmalanco <Jcmalanco>
2026-02-28 07:27:10 -0700

    no quite el app use de webhook :v

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.get('/', (req, res) => {
 app.use('/boletas', boletasRouter);
 app.use('/users', usersRouter);
 app.use('/pagos', pagosRouter);
-app.use('/webhooks', webhooksRouter);
+//app.use('/webhooks', webhooksRouter);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
